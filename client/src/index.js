70e043f52f5d5a6b3d429536f8b8b9b2172c4a03
Kit Jonathan Te <kjonathante@gmail.com>
2018-10-14 15:13:20 -0700

    Apollo Boost migration - required for subscriptions source: https://www.apollographql.com/docs/react/advanced/boost-migration.html source: https://www.apollographql.com/docs/react/advanced/subscriptions.html

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,37 @@ import "./index.css";
 // import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import ApolloClient from "apollo-boost";
+// import ApolloClient from "apollo-boost";
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
 import gql from "graphql-tag";
 import { ApolloProvider } from "react-apollo";
 import { Query } from "react-apollo";
 
+// const client = new ApolloClient({
+//   uri: "http://localhost:4000/graphql"
+// });
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: 'http://localhost:4000/graphql',
+      credentials: 'same-origin'
+    })
+  ]),
+  cache: new InMemoryCache()
 });
 
 const Posts = () => (
